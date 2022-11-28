67e88f453f38d1e6a28b766567605a29bcb26cc3
Guilherme Tchornei <guilhermetchornei@gmail.com>
2022-11-28 16:29:07 -0300

    Jogo completo (sem bonus) - corrigido

--- src/Components/Letras.js
+++ src/Components/Letras.js
@@ -2,7 +2,7 @@ export default function Letras(props) {
     return (
         <div className="letras">
             {props.alfabeto.map((l, index) =>
-                <button data-test="letter" key={index} className={props.listaLetras.includes(l) ? "selecionada" : ""}
+                <button data-test="letter" key={index} className={props.listaLetras.includes(l) ? "selecionada" : ""} disabled={props.listaLetras.includes(l) ? "selecionada" : ""}
                     onClick={() => props.selecionaLetra(l)}>{l.toUpperCase()}
                 </button>)}
         </div>
